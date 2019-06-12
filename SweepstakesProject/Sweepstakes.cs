d7c81505b221a122704286b6884eff0bfc0cb493
Nate Poths <pothsn@gmail.com>
2019-06-12 11:45:49 -0500

    Declared sweepstakes name and developed RegisterContestant method using UserInterface methods for obtaining user input.

--- SweepstakesProject/Sweepstakes.cs
+++ SweepstakesProject/Sweepstakes.cs
@@ -9,20 +9,22 @@ namespace SweepstakesProject
     public class Sweepstakes
     {
         //Member variables
-        Dictionary<string, string> test = new Dictionary<string, string>;
+        Dictionary<string, string> test = new Dictionary<string, string>();
+        string name;
 
         //Constructor
         public Sweepstakes(string name)
         {
-
+            name = "Super Duper Random Sweepstakes";
         }
 
         //Member methods
-
-
         public void RegisterContestant(Contestant contestant) //Adding to dictionary
         {
-
+            contestant.firstName = UserInterface.GetFirstName();
+            contestant.lastName = UserInterface.GetLastName();
+            contestant.emailAddress = UserInterface.GetEmailAddress();
+            contestant.registrationNumber = UserInterface.GetRegistrationNumber();
         }
 
         public string PickWinner() //Using dictionary
